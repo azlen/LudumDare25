e40db6bfc2a4da9198461bcdb18b150adfe2dc05
azlen <azlen@livingcode.org>
2012-12-14 08:18:50 -0800

    trying to fix audio.js

--- audio.js
+++ audio.js
@@ -67,53 +67,19 @@ function loadSound(soundFileName, loop, autoplay){
 		if (this.isPlaying){
 			this.stop();
 		}else{
-			this.play(0);
-		}
+			this.play(0, 0, 0);
+		},
 	}
 	return sound
 }
 
 
-var background = loadSound('horror_ambience.wav', true, true);
-var monster = loadSound('monster-8.wav');
-var x = 0, y = 0, z = 0;
-// In the frame handler function, get the object's position.
-// And copy the position over to the sound of the object.
-document.addEventListener('keydown', function(evt){
-	if (evt.keyCode === 65 || evt.keyCode === 37){
-		// 'w' or left arrow
-		x -= 0.1;
-	}else if(evt.keyCode === 68 || evt.keyCode === 39){
-		// 'd' or right arrow
-		x += 0.1;
-		background.panner.setPosition(x, 0, 0);
-	}else if(evt.keyCode === 87|| evt.keyCode === 38){
-		// 'w' or up arrow
-		if (evt.shiftKey){
-			z += 0.1;
-		}else{
-			y += 0.1;
-		}
-	}else if(evt.keyCode === 83 || evt.keyCode === 40){
-		// 's' or down arrow
-		if (evt.shiftKey){
-			z -= 0.1;
-		}else{
-			y -= 0.1;
-		}
-	}else if (evt.keyCode === 81){
-		// 'Q'
-		background.toggle();
-	}else if(evt.keyCode === 76){
-		// 'L'
-		monster.play(-2, 0, 2);
-	}else if(evt.keyCode === 82){
-		// R
-		monster.play(2);
-	}else{
-		console.log('evt.keyCode: %s', evt.keyCode);
-		return;
-	}
-	background.panner.setPosition(x,y,z);
-	console.log('x: %s, y: %s, z: %s', x, y, z);
-});
+var background = loadSound('monsters/beast/breathing.wav', true, true);
+var monster = loadSound('monsters/beast/wake.wav');
+
+var blood = loadSound('player/blood_splatter.wav');
+var scream = loadSound('player/scream.wav');
+
+setTimeout(function(){
+	blood.play(0, 0, 0)
+}, 5000)
