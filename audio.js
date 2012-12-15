2d1af5a3e8b2a66e16af802f47db55d31e951c48
azlen <azlen@livingcode.org>
2012-12-15 12:32:01 -0800

    started making the game

--- audio.js
+++ audio.js
@@ -6,80 +6,80 @@ window.AudioContext = (
 
 if (!AudioContext) {
   throw new Error("AudioContext not supported!");
-} 
-
-// Create a new audio context.
-var ctx = new AudioContext();
-
-// Create a AudioGainNode to control the main volume.
-var mainVolume = ctx.createGainNode();
-// Connect the main volume node to the context destination.
-mainVolume.connect(ctx.destination);
-
-
-function loadSound(soundFileName, loop, autoplay){
-	// Create an object with a sound source and a volume control.
-	var sound = {};
-	sound.isPlaying = false;
-	sound.loop = !!loop;
-	var request = new XMLHttpRequest();
-	request.open("GET", soundFileName, true);
-	request.responseType = "arraybuffer";
-	request.onload = function(e) {
-
-		// Create a buffer from the response ArrayBuffer.
-		sound.buffer = this.response;
-
-		// Make the sound source use the buffer and start playing it.
-		if (autoplay){
-			sound.play(0);
-		}
-	};
-	request.send();
-	sound.play = function(x, y, z){
-		sound.source = ctx.createBufferSource();
-		sound.volume = ctx.createGainNode();
-
-		// Connect the sound source to the volume control.
-		sound.source.connect(sound.volume);
-		// Hook up the sound volume control to the main volume.
-		// sound.volume.connect(mainVolume);
-
-		// Make the sound source loop.
-		sound.source.loop = this.loop;
-
-		// Load a sound file using an ArrayBuffer XMLHttpRequest.
-		sound.panner = ctx.createPanner();
-		// Instead of hooking up the volume to the main volume, hook it up to the panner.
-		sound.volume.connect(sound.panner);
-		// And hook up the panner to the main volume.
-		sound.panner.connect(mainVolume);
-		sound.panner.setPosition(x,y,z);
-		this.source.buffer = ctx.createBuffer(this.buffer, false);
-		this.source.noteOn(0); // this has been changed to .start() in the spec
-		sound.isPlaying = true;
-	}
-	sound.stop = function(){
-		sound.source.noteOff(0); // this has been changed to .stop() in the spec
-		sound.isPlaying = false;
-	}
-	sound.toggle = function(){
-		if (this.isPlaying){
-			this.stop();
-		}else{
-			this.play(0, 0, 0);
-		},
-	}
-	return sound
 }
 
+// Create a new audio context.
+var audio = new AudioContext();
 
-var background = loadSound('monsters/beast/breathing.wav', true, true);
-var monster = loadSound('monsters/beast/wake.wav');
+// Create a AudioGainNode to control the main volume.
+var mainVolume = audio.createGainNode();
+// Connect the main volume node to the context destination.
+mainVolume.connect(audio.destination);
 
-var blood = loadSound('player/blood_splatter.wav');
-var scream = loadSound('player/scream.wav');
 
-setTimeout(function(){
-	blood.play(0, 0, 0)
-}, 5000)
+function loadsound(soundFileName, loop, autoplay){
+        // Create an object with a sound source and a volume control.
+        var sound = {};
+        sound.isPlaying = false;
+        sound.loop = !!loop;
+        sound.x = 0;
+        sound.y = 0;
+        sound.z = 0;
+        var request = new XMLHttpRequest();
+        request.open("GET", soundFileName, true);
+        request.responseType = "arraybuffer";
+        request.onload = function(e) {
+
+                // Create a buffer from the response ArrayBuffer.
+                sound.buffer = this.response;
+
+                // Make the sound source use the buffer and start playing it.
+                if (autoplay){
+                        sound.play(0,0,0);
+                }
+        };
+        request.send();
+        sound.play = function(x, y, z){
+                sound.source = audio.createBufferSource();
+                sound.volume = audio.createGainNode();
+
+                // Connect the sound source to the volume control.
+                sound.source.connect(sound.volume);
+                // Hook up the sound volume control to the main volume.
+                // sound.volume.connect(mainVolume);
+
+                // Make the sound source loop.
+                sound.source.loop = this.loop;
+
+                // Load a sound file using an ArrayBuffer XMLHttpRequest.
+                sound.panner = audio.createPanner();
+                // Instead of hooking up the volume to the main volume, hook it up to the panner.
+                sound.volume.connect(sound.panner);
+                // And hook up the panner to the main volume.
+                sound.panner.connect(mainVolume);
+                sound.panner.setPosition(x,y,z);
+                this.source.buffer = audio.createBuffer(this.buffer, false);
+                this.source.noteOn(0); // this has been changed to .start() in the spec
+                sound.isPlaying = true;
+        }
+        sound.stop = function(){
+                sound.source.noteOff(0); // this has been changed to .stop() in the spec
+                sound.isPlaying = false;
+        }
+        sound.setPosition = function(x, y, z){
+        	this.x = x;
+        	this.y = y;
+        	this.z = z;
+        	if(this.panner){
+        		this.panner.setPosition(this.x, this.y, this.z);
+        	}
+        }
+        sound.toggle = function(){
+                if (this.isPlaying){
+                        this.stop();
+                }else{
+                        this.play(this.x, this.y, this.z);
+                }
+        }
+        return sound
+}
---
+++ game.js
@@ -0,0 +1,144 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
+var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+window.requestAnimationFrame = requestAnimationFrame;
+
+var mouseX = 0, mouseY, dragX, mousedown, nwse, frame = 0;
+
+var map = [
+	
+];
+
+var sounds = {
+	breathing: loadsound('objective.wav', true, true),
+	step1: loadsound('step1.wav'),
+	step2: loadsound('step2.wav')
+}
+
+var keys = {
+	w: false,
+	a: false,
+	s: false,
+	d: false
+}
+
+var player = {
+	x: 0,
+	z: 0,
+	speed: 1,
+	direction: 0
+}
+
+function draw(){
+	frame += 1;
+	canvas.width = window.innerWidth;
+  	canvas.height = window.innerHeight;
+  	
+  	for(nwse=-player.direction/4; nwse < -360; nwse+=360){}
+  	
+  	//draw minimal graphics
+  	ctx.fillStyle = '#999';
+  	ctx.fillRect(0, 0, canvas.width, canvas.height);
+  	ctx.fillStyle = '#555';
+  	ctx.font = 'bold 75px sans-serif';
+  	ctx.textAlign = 'center';
+  	ctx.fillText('Sound of Death', canvas.width/2, 75);
+  	ctx.font = '20px sans-serif';
+  	ctx.fillText('Works in chrome | Please use earphones', canvas.width/2, 100);
+  	ctx.fillStyle = '#777';
+  	ctx.fillRect(0, canvas.height/2, canvas.width, 10);
+  	ctx.fillStyle = '#555';
+  	ctx.font = '45px sans-serif';
+  	nwseAdd();
+  	ctx.fillText('N', (nwse/360)*(canvas.width*4), canvas.height/2+22.5);
+  	nwseAdd();
+  	ctx.fillText('W', (nwse/360)*(canvas.width*4), canvas.height/2+22.5);
+  	nwseAdd();
+  	ctx.fillText('S', (nwse/360)*(canvas.width*4), canvas.height/2+22.5);
+  	nwseAdd();
+  	ctx.fillText('E', (nwse/360)*(canvas.width*4), canvas.height/2+22.5);
+  	
+  	ctx.fillRect(player.x+canvas.width/2, player.z+ canvas.height/2, 10, 10);
+  	
+  	sounds.breathing.setPosition(Math.cos(mouseX/4)*(player.x/100), 0, Math.sin(mouseX/4)*(player.x/100));
+  	if(playerdistancetest(0, 0, 50) === true){
+  		console.log("yay");
+  	}
+  	
+  	
+  	//player functions
+  	if(keys.w === true){
+  		if(frame % 60 === 0){
+  			sounds.step1.volume = 0.3;
+  			sounds.step1.play(0, 0, 0);
+  		}else if(frame % 30 === 0){
+  			sounds.step2.play(0, 0, 0);
+  		}
+  		player.x += Math.cos(player.direction/(180 / Math.PI)/4 - (45/(180 / Math.PI)))*player.speed;
+  		player.z += Math.sin(player.direction/(180 / Math.PI)/4 - (45/(180 / Math.PI)))*player.speed;
+  	}
+  	
+  	requestAnimationFrame(draw);
+}
+
+function nwseAdd(){
+	nwse+=90;
+	if(nwse> 360){
+		nwse-=360;
+	}
+}
+
+function playerdistancetest(x, z, distance){
+	if(player.x < x+distance && player.x > x-distance && player.z > z+distance && player.z > z-distance){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+document.onmousemove = function(event){
+	mouseX = event.offsetX;
+	mouseY = event.offsetY;
+	if(mousedown === true){
+		player.direction -= dragX - event.offsetX;
+		dragX = event.offsetX;
+	}
+}
+
+document.onmousedown = function(event){
+	dragX = event.offsetX;
+	mousedown = true;
+}
+
+document.onmouseup = function(event){
+	mousedown = false;
+}
+
+
+document.onkeydown = function(event){
+	//console.log(event.keyCode);
+	if(event.keyCode === 87){
+		keys.w = true;
+	}else if(event.keyCode === 65){
+		keys.a = true;
+	}else if(event.keyCode === 83){
+		keys.s = true;
+	}else if(event.keyCode === 68){
+		keys.d = true;
+	}
+}
+
+document.onkeyup = function(event){
+	if(event.keyCode === 87){
+		keys.w = false;
+	}else if(event.keyCode === 65){
+		keys.a = false;
+	}else if(event.keyCode === 83){
+		keys.s = false;
+	}else if(event.keyCode === 68){
+		keys.d = false;
+	}
+}
+
+draw();
